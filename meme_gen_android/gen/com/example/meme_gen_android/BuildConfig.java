1f4a36344efdf544d23062ee9f9727f0d93be4c4
youcaughtafish <dzmoore@gmail.com>
2012-10-20 18:50:21 -0400

    moved server to it's own folder; created android app

---
+++ meme_gen_android/gen/com/example/meme_gen_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.meme_gen_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ meme_gen_android/src/com/example/meme_gen_android/CreateMemeActivity.java
@@ -0,0 +1,20 @@
+package com.example.meme_gen_android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CreateMemeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_meme);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_create_meme, menu);
+        return true;
+    }
+}
