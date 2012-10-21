27aa8180056d29b98ced10885263618c8387be5a
Dylan Moore <dzmoore@gmail.com>
2012-10-21 18:43:54 -0400

    refactored image view into meme_view.xml; still working on laying out components properly

--- meme_gen_android/gen/com/example/meme_gen_android/R.java
+++ meme_gen_android/gen/com/example/meme_gen_android/R.java
@@ -10,6 +10,9 @@ package com.example.meme_gen_android;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int zero_dip=0x7f040000;
+    }
     public static final class drawable {
         public static final int config_icon=0x7f020000;
         public static final int ic_action_search=0x7f020001;
@@ -18,42 +21,46 @@ public final class R {
         public static final int tmimitw=0x7f020004;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f070000;
-        public static final int bottom_text_linear=0x7f07000a;
-        public static final int bottom_text_row=0x7f070009;
-        public static final int config_bottom_text_btn=0x7f07000c;
-        public static final int config_top_text_btn=0x7f070008;
-        public static final int edit_bottom_text_btn=0x7f07000b;
-        public static final int edit_top_text_btn=0x7f070007;
-        public static final int image_linear=0x7f07000e;
-        public static final int image_row=0x7f07000d;
-        public static final int image_view=0x7f07000f;
-        public static final int menu_settings=0x7f070010;
-        public static final int save_btn=0x7f070004;
-        public static final int share_btn=0x7f070003;
-        public static final int share_save_linear=0x7f070002;
-        public static final int share_save_row=0x7f070001;
-        public static final int top_text_linear=0x7f070006;
-        public static final int top_text_row=0x7f070005;
+        public static final int TableLayout1=0x7f080000;
+        public static final int bottom_text_linear=0x7f08000a;
+        public static final int bottom_text_row=0x7f080009;
+        public static final int bottom_text_textview=0x7f080012;
+        public static final int config_bottom_text_btn=0x7f08000c;
+        public static final int config_top_text_btn=0x7f080008;
+        public static final int edit_bottom_text_btn=0x7f08000b;
+        public static final int edit_top_text_btn=0x7f080007;
+        public static final int image_relative_layout=0x7f08000e;
+        public static final int image_row=0x7f08000d;
+        public static final int image_view=0x7f080010;
+        public static final int meme_view_rel_layout=0x7f08000f;
+        public static final int menu_settings=0x7f080013;
+        public static final int save_btn=0x7f080004;
+        public static final int share_btn=0x7f080003;
+        public static final int share_save_linear=0x7f080002;
+        public static final int share_save_row=0x7f080001;
+        public static final int top_text_linear=0x7f080006;
+        public static final int top_text_row=0x7f080005;
+        public static final int top_text_textview=0x7f080011;
     }
     public static final class layout {
         public static final int activity_create_meme=0x7f030000;
+        public static final int meme_view=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_create_meme=0x7f060000;
+        public static final int activity_create_meme=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int create_meme_top_row_height=0x7f040004;
-        public static final int edit_bottom_text_string=0x7f040008;
-        public static final int edit_top_text_string=0x7f040007;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int save_string=0x7f040006;
-        public static final int share_string=0x7f040005;
-        public static final int title_activity_create_meme=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int create_meme_top_row_height=0x7f050004;
+        public static final int edit_bottom_text_string=0x7f050008;
+        public static final int edit_top_text_string=0x7f050007;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int save_string=0x7f050006;
+        public static final int share_string=0x7f050005;
+        public static final int title_activity_create_meme=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
