46687efb70fb9aa4d85ef7345d001aab29bae8a0
Dylan Moore <dzmoore@gmail.com>
2012-10-20 23:58:14 -0400

    began working on layout for create activity

--- meme_gen_android/gen/com/example/meme_gen_android/R.java
+++ meme_gen_android/gen/com/example/meme_gen_android/R.java
@@ -11,11 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int config_icon=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int tmimitw=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TableLayout1=0x7f070000;
+        public static final int bottom_text_linear=0x7f07000a;
+        public static final int bottom_text_row=0x7f070009;
+        public static final int config_bottom_text_btn=0x7f07000c;
+        public static final int config_top_text_btn=0x7f070008;
+        public static final int edit_bottom_text_btn=0x7f07000b;
+        public static final int edit_top_text_btn=0x7f070007;
+        public static final int image_linear=0x7f07000e;
+        public static final int image_row=0x7f07000d;
+        public static final int image_view=0x7f07000f;
+        public static final int menu_settings=0x7f070010;
+        public static final int save_btn=0x7f070004;
+        public static final int share_btn=0x7f070003;
+        public static final int share_save_linear=0x7f070002;
+        public static final int share_save_row=0x7f070001;
+        public static final int top_text_linear=0x7f070006;
+        public static final int top_text_row=0x7f070005;
     }
     public static final class layout {
         public static final int activity_create_meme=0x7f030000;
