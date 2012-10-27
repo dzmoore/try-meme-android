8c1e2f41da5f4960bacbd06edaa3dc776973623a
Dylan Moore <dzmoore@gmail.com>
2012-10-27 09:59:36 -0400

    added sql_init file

---
+++ meme_gen_server/docs/db_init.sql
@@ -0,0 +1,10 @@
+create table user (
+    id int(21) not null auto_increment,
+    username varchar(40) not null,
+    password varchar(60) not null,
+    active tinyint(1),
+    last_mod timestamp default current_timestamp on update current_timestamp,
+    salt varchar(40),
+    primary key (id)
+) engine=InnoDB;	
+
