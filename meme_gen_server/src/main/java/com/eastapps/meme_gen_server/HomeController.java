de6fb00554ad95b639104682db2a4dde16ac7e7c
dylan <dzmoore@gmail.com>
2012-11-02 10:07:38 -0400

    added background image getter method into HomeController

--- meme_gen_server/src/main/java/com/eastapps/meme_gen_server/HomeController.java
+++ meme_gen_server/src/main/java/com/eastapps/meme_gen_server/HomeController.java
@@ -1,15 +1,23 @@
 package com.eastapps.meme_gen_server;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,70 +28,118 @@ import com.eastapps.meme_gen_server.domain.User;
  */
 @Controller
 public class HomeController {
-	private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
-	
-	@Autowired
-	private SessionFactory sessionFactory;
-	
-	@Autowired
-	public HomeController(final SessionFactory sessionFactory) {
-	    this.sessionFactory = sessionFactory;
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public HomeController(final SessionFactory sessionFactory) {
+	this.sessionFactory = sessionFactory;
     }
 
-	/**
-	 * Selects the home page and populates the model with a message
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-		logger.info("Welcome home!");
-		model.addAttribute("controllerMessage",
-				"This is the message from the controller!");
-		return "home";
-	}
-	
-	@RequestMapping(value = "/hibtest", method = RequestMethod.GET)
-	public String doHibTest(Model model) {
-	    final Session session = sessionFactory.openSession();
-	   
-	    try {
-    	    session.beginTransaction();
-    	    
-    	    final StringBuilder sb = new StringBuilder();
-    	    final List<?> results = session.createQuery("from User").list();
-    	    boolean isNotFirst = false;
-    	    for (final Object eaResult : results) {
-    	        if (eaResult instanceof User) {
-    	            if (isNotFirst) {
-    	                sb.append("<br/>");
-    	            }
-    	            
-    	            final User eaUser = (User)eaResult;
-    	            
-    	            sb.append(eaUser.getUsername());
-    	            
-    	            isNotFirst = true;
-    	        }
-    	    }
-    	    
-    	    
-    	    model.addAttribute(
-    	        "controllerMessage",
-    	        sb.toString()
-            );
-    	    if (session != null) {
-	            if (session.getTransaction() != null) {
-        	        session.getTransaction().commit();
-	            }
-    	    }
-    	    
-	    } finally {
-	        if (session != null) {
-        	    session.close();
-	        }
+    /**
+     * Selects the home page and populates the model with a message
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+	logger.info("Welcome home!");
+	model.addAttribute("controllerMessage", "This is the message from the controller!");
+	return "home";
+    }
+
+    @RequestMapping(value = "/hibtest", method = RequestMethod.GET)
+    public String doHibTest(Model model) {
+	final Session session = sessionFactory.openSession();
+
+	try {
+	    session.beginTransaction();
+
+	    final StringBuilder sb = new StringBuilder();
+	    final List<?> results = session.createQuery("from User").list();
+	    boolean isNotFirst = false;
+	    for (final Object eaResult : results) {
+		if (eaResult instanceof User) {
+		    if (isNotFirst) {
+			sb.append("<br/>");
+		    }
+
+		    final User eaUser = (User) eaResult;
+
+		    sb.append(eaUser.getUsername());
+
+		    isNotFirst = true;
+		}
 	    }
-	    
-	    return "hibtest";
+
+	    model.addAttribute("controllerMessage", sb.toString());
+	    if (session != null) {
+		if (session.getTransaction() != null) {
+		    session.getTransaction().commit();
+		}
+	    }
+
+	} finally {
+	    if (session != null) {
+		session.close();
+	    }
 	}
+
+	return "hibtest";
+    }
+
+    @RequestMapping(value = "/meme_data/{id}/background", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getMemeBackground(@PathVariable("id") final String backgroundId) throws IOException {
+	final File img = new File("docs/scripts/tmimitw.jpg");
+	
+	final HttpHeaders responseHeaders = httpHeaderExcelFileAttachment(img.getName(), (int) img.length());
+	
+	return new ResponseEntity<byte[]>(getBytesFromFile(img), responseHeaders, HttpStatus.OK);
+    }
+
+    public static HttpHeaders httpHeaderExcelFileAttachment(final String fileName, final int fileSize) {
+	String encodedFileName = fileName.replace('"', ' ').replace(' ', '_');
+
+	HttpHeaders responseHeaders = new HttpHeaders();
+	responseHeaders.setContentType(MediaType.parseMediaType("image/jpeg"));
+	responseHeaders.setContentLength(fileSize);
+	responseHeaders.set("Content-Disposition", "attachment");
+	responseHeaders.add("Content-Disposition", "filename=\"" + encodedFileName + '\"');
+	return responseHeaders;
+    }
+
+    public static byte[] getBytesFromFile(File file) throws IOException {
+	InputStream is = new FileInputStream(file);
+
+	// Get the size of the file
+	long length = file.length();
+
+	if (length > Integer.MAX_VALUE) {
+	    // File is too large
+	    throw new IOException("File is too large");
+	}
+
+	// Create the byte array to hold the data
+	byte[] bytes = new byte[(int) length];
+
+	// Read in the bytes
+	int offset = 0;
+	int numRead = 0;
+	while (offset < bytes.length && 
+		(numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) 
+	{
+	    offset += numRead;
+	}
+
+	// Ensure all the bytes have been read in
+	if (offset < bytes.length) {
+	    throw new IOException("Could not completely read file " + file.getName());
+	}
+
+	// Close the input stream and return bytes
+	is.close();
+	
+	return bytes;
+    }
 
 }
--- meme_gen_server/src/test/java/com/eastapps/meme_gen_server/HomeControllerTest.java
+++ meme_gen_server/src/test/java/com/eastapps/meme_gen_server/HomeControllerTest.java
@@ -22,48 +22,49 @@ import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
 public class HomeControllerTest {
-	private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-	@Ignore
-	@Test
-	public void testController() {
-		HomeController controller = new HomeController(sessionFactory);
-		Model model = new ExtendedModelMap();
-		Assert.assertEquals("home",controller.home(model));
-		
-		Object message = model.asMap().get("controllerMessage");
-		Assert.assertEquals("This is the message from the controller!",message);
-		
-	}
-	
-	@Ignore
-	@Test
-	public void testHib() {
-	    final Session sesh = sessionFactory.openSession();
-	    sesh.beginTransaction();
-	    
-	    final List<?> users = sesh.createQuery("from User").list();
-	    TestCase.assertTrue(users.size() >= 1);
-	}
+    @Ignore
+    @Test
+    public void testController() {
+	HomeController controller = new HomeController(sessionFactory);
+	Model model = new ExtendedModelMap();
+	Assert.assertEquals("home", controller.home(model));
+
+	Object message = model.asMap().get("controllerMessage");
+	Assert.assertEquals("This is the message from the controller!", message);
+
+    }
+
+    @Ignore
+    @Test
+    public void testHib() {
+	final Session sesh = sessionFactory.openSession();
+	sesh.beginTransaction();
+
+	final List<?> users = sesh.createQuery("from User").list();
+	TestCase.assertTrue(users.size() >= 1);
+    }
 
     @Before
     public void setUp() throws Exception {
-        Resource rsrc = new FileSystemResource(new File("src/test/resources/test-context.xml"));
-        BeanFactory fac = new XmlBeanFactory(rsrc);
-//        LocalContainerEntityManagerFactoryBean mgrFacBen 
-//            = (LocalContainerEntityManagerFactoryBean) fac.getBean("entityManagerFactory");
-        
-        
-        // A SessionFactory is set up once for an application
-        sessionFactory = (SessionFactory)fac.getBean("mySessionFactory");
-                //.configure() // configures settings from hibernate.cfg.xml
-//                .buildSessionFactory();
+	Resource rsrc = new FileSystemResource(new File(
+		"src/test/resources/test-context.xml"));
+	BeanFactory fac = new XmlBeanFactory(rsrc);
+	// LocalContainerEntityManagerFactoryBean mgrFacBen
+	// = (LocalContainerEntityManagerFactoryBean)
+	// fac.getBean("entityManagerFactory");
+
+	// A SessionFactory is set up once for an application
+	sessionFactory = (SessionFactory) fac.getBean("mySessionFactory");
+	// .configure() // configures settings from hibernate.cfg.xml
+	// .buildSessionFactory();
     }
 
     @After
     public void tearDown() throws Exception {
-        if ( sessionFactory != null ) {
-            sessionFactory.close();
-        }
+	if (sessionFactory != null) {
+	    sessionFactory.close();
+	}
     }
 }
