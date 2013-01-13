4afee2fb2590fd48ce12d3201536fa38e1864585
dylan <dzmoore@gmail.com>
2013-01-13 18:08:34 -0500

    cacheing/fav'ing types in the type list appears to be working

--- meme_gen_android/src/com/eastapps/meme_gen_android/mgr/ICallback.java
+++ meme_gen_android/src/com/eastapps/meme_gen_android/mgr/ICallback.java
@@ -1,5 +1,9 @@
 package com.eastapps.meme_gen_android.mgr;
 
-public interface ICallback<T> {
-	public void callback(T obj);
+public class ICallback<T> {
+	public ICallback() {
+		super();
+	}
+	
+	public void callback(T obj) {}
 }
