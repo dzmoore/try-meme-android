5b53c81259a3fefc3b5006baf16358c2a43f5e7b
dylan <dzmoore@gmail.com>
2013-03-31 00:37:50 -0400

    reddit link traversal combined with quickmeme parsing; next, need to add the other quickmeme link types

--- meme_web_reader/meme_web_reader.py
+++ meme_web_reader/meme_web_reader.py
@@ -29,6 +29,7 @@ def get_meme_name(fullPageUrl):
 	return regex.findall(fullPageUrl)[0]
 
 def traverse_reddit_links(pageSoup):
+	links = set([])
 	regex = re.compile('http://www.quickmeme.com/meme/.+/')
 	hrefStr = ""
 	for ea in pageSoup.find_all("a"):
@@ -37,51 +38,62 @@ def traverse_reddit_links(pageSoup):
 			if len(hrefStr) > 0:
 				matches = regex.findall(hrefStr)
 				if len(matches) > 0:
-					print hrefStr
+					links.add(hrefStr)
+	return links
 
+
+def print_meme_detail(url):
+	# set up some variables for this script
+	# (these should change for different sites)
+	baseUrl = 'http://www.quickmeme.com'
+	captionLinkId = 'editurl'
+	apiCaptionUrl = '/make/get_data/?id='
+
+	
+	# get the text for the page at 'url'
+	# and initialize the BeautifulSoup object for it
+	pageOneText = get_page_text(url)
+	pageOneSoup = BeautifulSoup(pageOneText)
+
+	# retrieve the link for the 'add your own caption' page
+	pageTwoLinkUrl 	= get_caption_url(pageOneSoup, captionLinkId, baseUrl)
+	
+	# create the url of the api call to retrieve the
+	# captions (top/bottom text)
+	captionApiUrl = get_caption_api_url(baseUrl, apiCaptionUrl, pageTwoLinkUrl)
+	
+	# call the api and process the response
+	jsonCaptionResponse = json.loads(get_page_text(captionApiUrl))
+	jsonCaps = jsonCaptionResponse['caps']
+	jsonTopCap = jsonCaps[0]
+	jsonBottomCap = jsonCaps[1]
+
+	topTxt = jsonTopCap['txt']
+	bottomTxt = jsonBottomCap['txt']
+
+	# get the name from the caption page url
+	memeName = get_meme_name(pageTwoLinkUrl)
+
+	print 'URL:\t\t\t' + url
+	print 'Caption URL:\t\t' + captionApiUrl 
+	print 'Meme Name:\t\t' + memeName
+	print 'Top Text:\t\t' + topTxt
+	print 'Bottom Text:\t\t' + bottomTxt
+	
+	return 0
 
 def main():
-		# set up some variables for this script
-		# (these should change for different sites)
-		baseUrl = 'http://www.quickmeme.com'
-		captionLinkId = 'editurl'
-		apiCaptionUrl = '/make/get_data/?id='
+	# first argument should be meme page url
+	args = sys.argv[1:]
+	#urlArg = args[0]
 
-		# first argument should be meme page url
-		args = sys.argv[1:]
-		url = args[0]
-		
-		# get the text for the page at 'url'
-		# and initialize the BeautifulSoup object for it
-		pageOneText = get_page_text(url)
-		pageOneSoup = BeautifulSoup(pageOneText)
+	linksFromReddit = traverse_reddit_links(BeautifulSoup(get_page_text('http://www.reddit.com/r/AdviceAnimals/top/?sort=top&t=hour')))
 
-		# retrieve the link for the 'add your own caption' page
-		pageTwoLinkUrl 	= get_caption_url(pageOneSoup, captionLinkId, baseUrl)
-		
-		# create the url of the api call to retrieve the
-		# captions (top/bottom text)
-		captionApiUrl = get_caption_api_url(baseUrl, apiCaptionUrl, pageTwoLinkUrl)
-		
-		# call the api and process the response
-		jsonCaptionResponse = json.loads(get_page_text(captionApiUrl))
-		jsonCaps = jsonCaptionResponse['caps']
-		jsonTopCap = jsonCaps[0]
-		jsonBottomCap = jsonCaps[1]
+	for ea in linksFromReddit:
+		print_meme_detail(ea)
+		print("\n")
 
-		topTxt = jsonTopCap['txt']
-		bottomTxt = jsonBottomCap['txt']
+main()
+					
 
-		# get the name from the caption page url
-		memeName = get_meme_name(pageTwoLinkUrl)
 
-		print 'URL:\t\t\t' + url
-		print 'Caption URL:\t\t' + captionApiUrl 
-		print 'Meme Name:\t\t' + memeName
-		print 'Top Text:\t\t' + topTxt
-		print 'Bottom Text:\t\t' + bottomTxt
-		
-		return 0
-
-#main()
-traverse_reddit_links(BeautifulSoup(get_page_text('http://www.reddit.com/r/AdviceAnimals/top/?sort=top&t=hour')))
