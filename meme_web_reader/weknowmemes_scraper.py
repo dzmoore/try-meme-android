b78ebce41af7b1c990a60c14862d338824d3e9f7
dylan <dzmoore@gmail.com>
2013-04-11 22:05:15 -0400

    figured out how to get img from file and write to file..but still need to direct scraper into generator page, rather than link img

--- meme_web_reader/weknowmemes_scraper.py
+++ meme_web_reader/weknowmemes_scraper.py
@@ -12,10 +12,63 @@ def get_page_text(url):
     pageResults.close()
     return pageText
 
-def main():
-	wkmText = get_page_text('http://weknowmemes.com/generator/ajax/get_meme.php?page=2')
-	print wkmText
+def getWeKnowMemesPageText(page_num):
+	wkmText = get_page_text('http://weknowmemes.com/generator/ajax/get_meme.php?page=' + str(page_num))
+	return wkmText
 
+def main():
+	for i in range(1, 2):
+		wkmText = getWeKnowMemesPageText(i)
+		print(wkmText)
+
+		wkmBSoup = BeautifulSoup(wkmText)
+
+		for eaImgTag in wkmBSoup.find_all('img'):
+			imgUrl = eaImgTag.attrs['src']
+			imgName = eaImgTag.attrs['data-title']
+			
+			opener = urllib2.build_opener()
+			page = opener.open(imgUrl)
+			img = page.read()
+
+			imgFilenameRegex = re.compile(
+				'(?<=http://weknowmemes.com/generator/uploads/generated/).+'
+			)
+
+			regexGroup = imgFilenameRegex.findall(imgUrl)
+		
+			if (len(regexGroup) > 0):
+				imgFilename = regexGroup[0]
+				imgFilenameSansSuffixRegex = re.compile(
+					'.+(?=\\.)'
+				);
+				
+				imgFilenameGroup = imgFilenameSansSuffixRegex.findall(
+					imgFilename		
+				)
+				
+				if (len(imgFilenameGroup) > 0):
+					imgFileSansSuffix = imgFilenameGroup[0]
+				
+					print(
+						'img file name sans suffix: '
+						+ str(imgFileSansSuffix)
+					)
+					
+					filename = imgFileSansSuffix + 'png'
+					fout = open(filename, 'wb')
+					fout.write(img)
+					fout.close()
+
+					print('stored: ' + filename)	
+					
+					#ohh wait, need to go into the actual 'generator' page using the href links
+					
+				else:
+					print('no img filename: ' + str(imgFilename))
+
+			else:
+				print('no filename: ' + str(regexGroup))
 
 main()
 
