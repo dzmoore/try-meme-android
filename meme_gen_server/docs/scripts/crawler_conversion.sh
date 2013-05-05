f58046dedb8282f095d6cb12eacbac66fb31d768
dylan <dzmoore@gmail.com>
2013-05-05 14:38:13 -0400

    meme crawler conversion query should now be working

--- meme_gen_server/docs/scripts/crawler_conversion.sh
+++ meme_gen_server/docs/scripts/crawler_conversion.sh
@@ -67,32 +67,41 @@ insert into meme
 		(
 			select
 				auto_increment + ((@curRow := @curRow + 1) * 10)
-			from informatin_schema.tables
+			from information_schema.tables
 			where
 				table_name = 'meme'
 				and table_schema = 'heroku_b630f8d967bd52a'	
-		)
-		meme_bg.fk				as meme_background_fk,
-		meme_type.fk			as lv_meme_type_fk,
+		)						as id,
+		meme_bg.id				as meme_background_fk,
+		meme_type.id			as lv_meme_type_fk,
 		1						as created_by_user_fk,
 		1						as is_sample_meme,
 		current_timestamp		as last_mod
 	from
 		(
-			select distinct
-				cb.crawler_img_desc as desc
-			from
-				crawler_meme cm join crawler_background cb
-				on (upper(cm.name) = upper(cb.crawler_img_desc))
-		) as crawler_bg_desc
+            select distinct
+                cb.crawler_img_desc as descr
+            from
+                crawler_meme cm join crawler_background cb
+                on (upper(cm.name) = upper(cb.crawler_img_desc))
+        ) as crawler_bg_descr
 
-		join 
+        join
 
-		(
-			select distinct
-				crawler_img_filename as filename,
-				crawler_img_desc 	 as desc
-			from
-				crawler_background
-		) as crawler_bg_filename
-		on (upper(crawler_bg_desc.desc) = upper(crawler_bg_filename.desc)
+        (
+            select distinct
+                crawler_img_filename as filename,
+				crawler_img_desc     as descr,
+                id     		     	 as id
+            from
+                crawler_background
+        ) as crawler_bg_filename
+        on (upper(crawler_bg_descr.descr) = upper(crawler_bg_filename.descr))
+
+		join meme_background meme_bg
+		on (upper(meme_bg.path) = upper(crawler_bg_filename.filename))
+
+		join lv_meme_type meme_type
+		on (upper(meme_type.descr) = upper(crawler_bg_descr.descr))
+
+		join (select @curRow := 1) r
