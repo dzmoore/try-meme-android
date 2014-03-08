ad85c6b7db908216b1e708044ecbd5fc3a34f8e1
Dylan Moore <dzmoore@gmail.com>
2014-03-08 18:20:52 -0500

    moved android files to root dir

--- gen/com/trymeme/meme_gen_android/BuildConfig.java
+++ gen/com/trymeme/meme_gen_android/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.eastapps.meme_gen_android.test;
+package com.trymeme.meme_gen_android;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
